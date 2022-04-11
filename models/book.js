a9f49ddc753211ce6da59d47f110c6c744174ec2
RyanHPaez <solosnail@gmail.com>
2022-04-10 20:49:27 -0700

    books

---
+++ models/book.js
@@ -0,0 +1,17 @@
+//dependencies and requires
+const mongoose = require('mongoose')
+const {Schema} = mongoose 
+
+//schema
+const bookSchema = new Schema ({
+    title: String,
+    description: String,
+    year: Number,
+    quantity: Number,
+    imageURL: String
+})
+
+
+//model and export
+const Book = mongoose.model('Book', bookSchema)
+module.exports = Book 
